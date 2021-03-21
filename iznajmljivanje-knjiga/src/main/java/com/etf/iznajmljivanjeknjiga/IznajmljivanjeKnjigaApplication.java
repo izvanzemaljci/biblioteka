a2e6c42a9152ba8392dc9a359fc3dd0c9835c1e0
iosmankovi1 <iosmankovi1@etf.unsa.ba>
2021-03-21 21:03:53 +0100

    insert example

--- iznajmljivanje-knjiga/src/main/java/com/etf/iznajmljivanjeknjiga/IznajmljivanjeKnjigaApplication.java
+++ iznajmljivanje-knjiga/src/main/java/com/etf/iznajmljivanjeknjiga/IznajmljivanjeKnjigaApplication.java
@@ -3,6 +3,7 @@ package com.etf.iznajmljivanjeknjiga;
 import java.util.ArrayList;
 
 import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -22,26 +23,20 @@ public class IznajmljivanjeKnjigaApplication {
 		MongoClient mongoClient =  MongoClients.create(
 				 "mongodb+srv://sanja:borkvOCiBpguBQtt@cluster0.7prvw.mongodb.net/biblioteka?retryWrites=true&w=majority");
 		 MongoDatabase database = mongoClient.getDatabase("biblioteka");
-		    MongoCollection<Document> collection = database.getCollection("login-korisnika");
-		    MongoCursor<Document> doc=collection.find().iterator() ;   
-		    while(doc.hasNext()) {
-		    	ArrayList<Object> var=new ArrayList<>(doc.next().values());
-		    	System.out.println(var.get(0));
-		    	System.out.println(var.get(1));
-		    	System.out.println(var.get(2));
-		    	System.out.println(var.get(3));
-		    }
-//		try(MongoClient mongoClient = new MongoClient(uri))
-//			{
-//				MongoDatabase database = mongoClient.getDatabase("test");			
-//				MongoCollection<Document> collection = database.getCollection("test");
-//				Document query = new Document("_id", new ObjectId("5e234fe121fcf183e83ddce2"));
-//		        Document result = collection.find(query).iterator().next();
-//
-//		        System.out.println("Test3: "+result.getString("test3"));
-//			}		
-//		
-//        SpringApplication.run(IznajmljivanjeKnjigaApplication.class, args);
+		MongoCollection<Document> collection = database.getCollection("login-korisnika");
+		
+		Document k3 = new Document("_id",new ObjectId());
+		k3.append("id", "4").append("username", "irma").append("password", "123").append("role", "korisnik") ;
+		collection.insertOne(k3);
+		MongoCursor<Document> doc=collection.find().iterator() ;   
+		while(doc.hasNext()) {
+	    	ArrayList<Object> var=new ArrayList<>(doc.next().values());
+	    	System.out.println(var.get(0));
+	    	System.out.println(var.get(1));
+	    	System.out.println(var.get(2));
+	    	System.out.println(var.get(3));
+		}
+		
 	}
 
 }
