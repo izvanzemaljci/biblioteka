4d78d9c9e3dce3543739848c4266bf56eac9d75c
almasafesta <afesta1@etf.unsa.ba>
2021-04-09 01:34:56 +0200

    Test

--- clanarina/src/test/java/com/etf/clanarina/ClanarinaTest.java
+++ clanarina/src/test/java/com/etf/clanarina/ClanarinaTest.java
@@ -1,22 +1,58 @@
 package com.etf.clanarina;
 
-import java.nio.charset.Charset;
+
+import static org.junit.Assert.*;
+
+
+import java.util.Optional;
 
 import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.util.Assert;
 
-import com.etf.clanarina.controller.clanarinaController;
+import com.etf.clanarina.model.Clanarina;
 import com.etf.clanarina.repository.ClanarinaRepository;
-import com.etf.clanarina.repository.KorisnikRepository;
+import org.junit.After;
+import org.junit.Before;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.*;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Field;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClanarinaTest  {
+	@Autowired
+    private ClanarinaRepository mongoTemplate;
+	
+	
+	@Test
+    public void execute() {
+		Clanarina clanarina=new Clanarina(8998L, 0L, "00/00/0000", 0.0);
+		mongoTemplate.insert(clanarina);
+		Optional <Clanarina> clanarina1= mongoTemplate.findById(8989L);
+		Assert.isTrue(clanarina1.equals(clanarina1), "must be true");
+
+    }
+	@AfterEach
+    public void destroy() {
+        mongoTemplate.delete(8998L);
+    }
+
+	
+}
+
+
+
 /*@RunWith(SpringRunner.class) 
 @WebMvcTest
 @AutoConfigureMockMvc
