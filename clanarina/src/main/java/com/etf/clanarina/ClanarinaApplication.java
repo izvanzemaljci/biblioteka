7514d5b3f962a6b3962cc6f68c2f3cbc3164831b
iosmankovi1 <iosmankovi1@etf.unsa.ba>
2021-04-09 14:41:32 +0200

    fix

--- clanarina/src/main/java/com/etf/clanarina/ClanarinaApplication.java
+++ clanarina/src/main/java/com/etf/clanarina/ClanarinaApplication.java
@@ -1,70 +1,23 @@
 package com.etf.clanarina;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-import com.mongodb.client.MongoDatabase;
-
-import com.etf.clanarina.repository.ClanarinaRepository;
-
+@EnableDiscoveryClient
 @EnableEurekaClient
 @SpringBootApplication
-public class ClanarinaApplication {
-	@Autowired
-	public static MongoDatabase database;
-	@Autowired
-	private ClanarinaRepository repository;
+public class ClanarinaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClanarinaApplication.class, args);
-		
-		/*
-		MongoClient mongoClient =  MongoClients.create("mongodb+srv://sanja:borkvOCiBpguBQtt@cluster0.7prvw.mongodb.net/biblioteka?retryWrites=true&w=majority");
-		MongoDatabase database = mongoClient.getDatabase("biblioteka");
-		MongoCollection<Document> connectionClanarina = database.getCollection("clanarina");
-		MongoCollection<Document> connectionKorisnik = database.getCollection("korisnik");
-		ObjectMapper oMapper = new ObjectMapper();
-		
-		List<Clanarina> insertClanarina = new ArrayList<>();
-		insertClanarina.add(new Clanarina(new Long(1), new Long(1), LocalDateTime.now(), new Double(0)));
-		insertClanarina.add(new Clanarina(new Long(2), new Long(2), LocalDateTime.now(), new Double(1.0)));
-		insertClanarina.add(new Clanarina(new Long(3), new Long(3), LocalDateTime.now(), new Double(2)));
-		insertClanarina.forEach(i->{
-			connectionClanarina.insertOne(new Document(oMapper.convertValue(i, Map.class)));
-		});
-		
-		List<Korisnik> insertKorisnik = new ArrayList<>();
-		insertKorisnik.add(new Korisnik(new Long(0), new String("Almasa"), new String("Festa"), LocalDateTime.now(), new Long(0)));
-		insertKorisnik.add(new Korisnik(new Long(1), new String("Sanja"), new String("Milovic"), LocalDateTime.now(), new Long(1)));
-		insertKorisnik.add(new Korisnik(new Long(2), new String("Irma"), new String("Osmankovic"), LocalDateTime.now(), new Long(2)));
-		insertKorisnik.add(new Korisnik(new Long(3), new String("Sara"), new String("Ramadanovic"), LocalDateTime.now(), new Long(3)));
-		insertKorisnik.forEach(i->{
-			connectionKorisnik.insertOne(new Document(oMapper.convertValue(i, Map.class)));
-		});
-		
-		System.out.println("Clanarina");
-		MongoCursor<Document> clanarina=connectionClanarina.find().iterator();
-		while(clanarina.hasNext()) {
-			ArrayList<Object> var =new ArrayList<> (clanarina.next().values());
-			System.out.println(var.get(0)+" "+var.get(1)+" "+var.get(2)+" "+var.get(3));
-		}
-		
-		System.out.println("Korisnik");
-		MongoCursor<Document> korisnik=connectionKorisnik.find().iterator();
-		while(korisnik.hasNext()) {
-			ArrayList<Object> var =new ArrayList<> (korisnik.next().values());
-			System.out.println(var.get(0)+" "+var.get(1)+" "+var.get(2)+" "+var.get(3)+" "+var.get(4));
-		}*/
-		
-	
 
-
-		
-		
 	}
 
-	
+	@Override
+	public void run(String... args) throws Exception {
+	}
+
 }
--- clanarina/src/main/java/com/etf/clanarina/model/Clanarina.java
+++ clanarina/src/main/java/com/etf/clanarina/model/Clanarina.java
@@ -1,8 +1,6 @@
 package com.etf.clanarina.model;
 
-import java.io.Serializable;
-import java.time.LocalDateTime;
-
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.validation.constraints.NotNull;
@@ -11,64 +9,63 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-
+@Entity
 @Document(collection = "clanarina")
 public class Clanarina {
-	
+
 	@Id
-	private ObjectId objectId;	
-	
+	private ObjectId objectId;
+
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
 	@NotNull(message = "IDkorisnika can't be null")
 	private Long idKorisnika;
-	
+
 	@NotNull(message = "DatumUpisa can't be null")
 	private String datumUpisa;
 	private Double platiti;
-	
+
 	public Clanarina() {
 		super();
 	}
-	public Clanarina(Long id, Long idKorisnika,  String datumUpisa, Double platiti) {
+
+	public Clanarina(Long id, Long idKorisnika, String datumUpisa, Double platiti) {
 		super();
 		this.id = id;
 		this.idKorisnika = idKorisnika;
 		this.datumUpisa = datumUpisa;
 		this.platiti = platiti;
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public Long getIdKorisnika() {
 		return idKorisnika;
 	}
+
 	public void setIdKorisnika(Long idKorisnika) {
 		this.idKorisnika = idKorisnika;
-	}	
+	}
+
 	public String getDatumUpisa() {
 		return datumUpisa;
 	}
+
 	public void setDatumUpisa(String datumUpisa) {
 		this.datumUpisa = datumUpisa;
-	}	
+	}
+
 	public Double getPlatiti() {
 		return platiti;
 	}
+
 	public void setPlatiti(Double platiti) {
 		this.platiti = platiti;
 	}
